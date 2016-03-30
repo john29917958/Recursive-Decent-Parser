76943c75c857ca3794ab0cf8eda8b8601496df7f
John Wang <john29917958@gmail.com>
2016-03-30 20:38:37 +0800

    Fix bugs and show matched tokens.

--- Recursive-Decent-Parser/Parser.cpp
+++ Recursive-Decent-Parser/Parser.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+void error_input_stream();
+void show_results();
+
 string EXPR(const string);
 string TERM(const string);
 string VAL(const string);
@@ -15,6 +18,8 @@ regex rparenthesis("^\\)");
 regex op("^[-+*\/]");
 regex num("^[+-]?(0|[1-9]\\d*)(\\.\\d+)?");
 regex sign("^[+-]");
+
+vector<string> results;
 
 int main()
 {
@@ -29,12 +34,30 @@ int main()
             break;
         }
 
+        results.clear();
+
         EXPR(current_line);
 
-        cout << "Passed" << endl;
-    }    
+        if (!results.empty())
+        {
+            show_results();
+        }
+        else
+        {
+            cout << "invalid input";
+            break;
+        }
+    }
 
     return 0;
+}
+
+void show_results()
+{
+    for (int i = 0; i < results.size(); i++)
+    {
+        cout << results[i] << endl;
+    }
 }
 
 void error_input_stream()
@@ -58,10 +81,14 @@ string TERM(const string string_stream)
 {
     string pending_string_stream = string_stream;
     string peek = pending_string_stream.substr(0, 1);
-    
-    if (regex_search(peek, op))
+    smatch match;
+
+    if (regex_search(peek, match, op))
     {
+        results.push_back("op " + match.str());
+
         pending_string_stream = regex_replace(pending_string_stream, op, "");
+        pending_string_stream = SIG(pending_string_stream);
         pending_string_stream = VAL(pending_string_stream);
         pending_string_stream = TERM(pending_string_stream);
 
@@ -71,8 +98,7 @@ string TERM(const string string_stream)
     if (string_stream.empty() ||
         regex_search(string_stream, rparenthesis))
     {
-        // do nothing.
-        return string_stream;
+        return string_stream; // do nothing.
     }
 
     error_input_stream();
@@ -82,19 +108,23 @@ string TERM(const string string_stream)
 string VAL(const string string_stream)
 {
     string pending_string_stream = string_stream;
+    smatch match;
 
-    if (regex_search(pending_string_stream, num))
+    if (regex_search(pending_string_stream, match, num))
     {
+        results.push_back("num " + match.str());
         return regex_replace(pending_string_stream, num, "");
     }
 
-    if (regex_search(pending_string_stream, lparenthesis))
+    if (regex_search(pending_string_stream, match, lparenthesis))
     {
+        results.push_back("lparenthesis " + match.str());
         pending_string_stream = regex_replace(pending_string_stream, lparenthesis, "");
         pending_string_stream = EXPR(pending_string_stream);
         
-        if (regex_search(pending_string_stream, rparenthesis))
+        if (regex_search(pending_string_stream, match, rparenthesis))
         {
+            results.push_back("rparenthesis " + match.str());
             pending_string_stream = regex_replace(pending_string_stream, rparenthesis, "");
             return pending_string_stream;
         }
@@ -111,17 +141,18 @@ string SIG(const string string_stream)
 {
     string pending_string_stream = string_stream;
     string peek = pending_string_stream.substr(0, 1);
+    smatch match;
 
-    if (regex_search(pending_string_stream, sign))
+    if (regex_search(pending_string_stream, match, sign))
     {
+        results.push_back("sign " + match.str());
         return regex_replace(pending_string_stream, sign, "");
     }
 
     if (regex_search(pending_string_stream, lparenthesis) ||
         regex_search(pending_string_stream, num))
     {
-        // do nothing.
-        return pending_string_stream;
+        return pending_string_stream; // do nothing.
     }
 
     error_input_stream();
