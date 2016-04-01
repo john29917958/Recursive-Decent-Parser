72c50257cad40a6702ea60d1a1d6669b1eb6e5fc
John Wang <john29917958@gmail.com>
2016-04-01 12:15:19 +0800

    Adds unit test project.

---
+++ ParserUnitTest/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ParserUnitTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 您的測試程式碼在這裡
+		}
+
+	};
+}
